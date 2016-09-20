079ef04a89bc7318446162f050b309b021b90f04
Diego Armando Castellanos Sabogal <castsabproyectos@gmail.com>
2016-09-20 10:21:14 -0500

    Se ajusta los estilos del carrusel

--- js/carousel.js
+++ js/carousel.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     $('.owl-carousel').owlCarousel({
         loop: true,
-        margin: 10,
+        margin: 15,
         nav: true,
         responsive: {
             0: {
                 items: 1
             },
             600: {
-                items: 3
+                items: 2
             },
             1000: {
-                items: 5
+                items: 3
             }
         }
     });
